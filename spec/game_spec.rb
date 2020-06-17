3b497bc96bc60ce0783c295605c34cd17eee22be
Gina Frankel <g.frankel22@gmail.com>
2020-06-17 18:15:41 +0100

    Fix: it statement in test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,26 +1,20 @@
+# frozen_string_literal: true
+
 require 'game'
-  describe Game do
-    describe '#play' do
-      it 'game can start' do
-        game = Game.new
+describe Game do
+  describe '#play' do
+    it 'game can start' do
+      game = Game.new
 
-        game.play
+      game.play
 
-        expect(game.playing).to eq true 
-      end 
-
-    end 
-    it 'game asks questions' do
-        game = Game.new
-
-        welcome_message = "Welcome to the Coding game"
-        expect(game.play).to eq welcome_message
-
-        
-      
+      expect(game.playing).to eq true
     end
- 
   end
+  it 'game gives welcome message' do
+    game = Game.new
 
-
-
+    welcome_message = 'Welcome to the Coding game'
+    expect(game.play).to eq welcome_message
+  end
+end
